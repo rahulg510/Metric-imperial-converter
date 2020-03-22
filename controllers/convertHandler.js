8753cd25d0c76555a0d5cb4608919ac54fe084f5
rahulg510 <rahulg510@live.com>
2020-03-21 22:42:14 -0700

    implemented conversion handler

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -9,44 +9,101 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    var result;
+    var regexStr = input.match(/[a-z]+|[^a-z]+/gi);
+    return parseFloat(regexStr[0]) || "Invalid Number";
     
-    return result;
   };
   
   this.getUnit = function(input) {
-    var result;
-    
-    return result;
+    var regexStr = input.match(/[a-z]+|[^a-z]+/gi);
+    return spellitOut(regexStr[1]);
+  
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+
+  switch(initUnit){
+    case 'miles':
+      return 'kilometers';
+    case 'kilometers':
+      return 'miles';
+    case 'pounds':
+      return 'kilograms';
+    case 'kilorgrams':
+      return 'pounds';
+    case 'gallons':
+      return 'liters';
+    case 'liters':
+      return 'gallons';
+
+    }
   };
 
-  this.spellOutUnit = function(unit) {
+ function spellitOut (initUnit) {
     var result;
-    
-    return result;
+    if(initUnit == 'mi' || initUnit == 'miles' || initUnit == 'mile'){
+      result = 'miles';
+     }
+    else if(initUnit == 'km' || initUnit == 'kilometers' || initUnit == 'kilometer'){
+      result = 'kilometers';
+     }
+    else if(initUnit == 'kg' || initUnit == 'kilograms' || initUnit == 'kilogram'){
+      result = 'kilograms';
+      }
+    else if(initUnit == 'lb' || initUnit == 'pounds' || initUnit == 'pound'){
+      result = 'pounds';
+     }
+    else if(initUnit == 'gal' || initUnit == 'gallons' || initUnit == 'gallons'){
+      result = 'gallons';
+    }
+    else if(initUnit == 'l' || initUnit == 'liters' || initUnit == 'liter'){
+      result = 'liters';
+    }
+    else{
+      result = "Invalid Unit";
+    }
+      return result;
   };
   
   this.convert = function(initNum, initUnit) {
+    var result;
+
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
     
+    switch(initUnit){
+      case 'miles':
+        result = initNum * miToKm;
+        break;
+      case 'kilometers': 
+        result = initNum / miToKm;
+        break;
+      case 'pounds':
+        result = initNum * lbsToKg;
+        break;
+      case 'kilograms':
+        result = initNum / lbsToKg;
+        break;
+      case 'gallons':
+        result = initNum * galToL;
+        break;
+      case 'liters':
+        result = initNum / galToL;
+        break;
+      }
     return result;
-  };
+  }
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
-  };
-  
+    var result = {initNum:  initNum,
+                  initUnit: initUnit,
+                  returnNum: returnNum,
+                  returnUnit: returnUnit, 
+                  string: "\'" + initNum + initUnit + "converts to " + returnNum + " " + returnUnit + "\'"
+  }
+  return result;
+}
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 var expect = require('chai').expect;
@@ -20,11 +12,20 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
+      if(initNum == 'Invalid Number' && initUnit == 'Invalid Unit'){
+        res.send("invalid number and unit");
+      }
+      else if(initNum == 'Invalid Number'){
+        res.send("invalid number");
+      }
+      else if(initUnit == 'Invalid Unit'){
+        res.send("invalid unit");
+      }
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
-      //res.json
+      res.json(toString);
     });
     
 };
